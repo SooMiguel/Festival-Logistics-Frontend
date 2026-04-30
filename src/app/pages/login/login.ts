1965fb9dda542f77879d8a1daf1d0727390d75c8
Fer <i202335112@cibertec.edu.pe>
2026-04-30 16:03:45 -0500

    feat: login, navbar y dashboard con tematica festival

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+  username = '';
+  password = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/dashboard']);
+  }
+}
