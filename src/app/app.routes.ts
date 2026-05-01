3db280f456e3f675d4d2c08e3f770de9c270300e
SooMiguel <miguelangeluriartepacheco@gmail.com>
2026-04-30 22:31:56 -0500

    fix: creacion de componentes base, rutas corregidas y tema Ultra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,14 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { MainLayout } from './layout/main-layout/main-layout';
 import { authGuard } from './guards/auth-guard';
 
+// Importamos las clases exactamente con el nombre que generó tu compañero
+import { Artistas } from './pages/artistas/artistas';
+import { Escenarios } from './pages/escenarios/escenarios';
+import { Equipos } from './pages/equipos/equipos';
+import { Presentaciones } from './pages/presentaciones/presentaciones';
+import { Usuarios } from './pages/usuarios/usuarios';
+import { NotFound } from './pages/not-found/not-found';
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
@@ -13,11 +21,13 @@ export const routes: Routes = [
     canActivate: [authGuard],
     children: [
       { path: 'dashboard', component: Dashboard },
-      { path: 'artistas', component: Dashboard },
-      { path: 'escenarios', component: Dashboard },
-      { path: 'equipos', component: Dashboard },
-      { path: 'presentaciones', component: Dashboard }
+      { path: 'artistas', component: Artistas },
+      { path: 'escenarios', component: Escenarios },
+      { path: 'equipos', component: Equipos },
+      { path: 'presentaciones', component: Presentaciones },
+      { path: 'usuarios', component: Usuarios }
     ]
   },
-  { path: '**', redirectTo: 'login' }
+  // Si alguien escribe una URL que no existe, lo mandamos al error 404
+  { path: '**', component: NotFound }
 ];
---
+++ src/app/pages/escenarios/escenarios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escenarios',
+  imports: [],
+  templateUrl: './escenarios.html',
+  styleUrl: './escenarios.css',
+})
+export class Escenarios {}
